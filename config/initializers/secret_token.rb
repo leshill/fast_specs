be4162e483b7f06636d04f1174fcf2c1c9df1d2e
Les Hill <leshill@gmail.com>
2011-10-23 14:20:31 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FastSpecs::Application.config.secret_token = 'eab63746788f6196f92978680797cc7e797296e175281106bb514fd6144c3f1f0f921a6f0bbb2faa912a299bda8c4b1c3b50d4668c627015d89cd259195600c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FastSpecs::Application.config.session_store :cookie_store, key: '_fast_specs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FastSpecs::Application.config.session_store :active_record_store
